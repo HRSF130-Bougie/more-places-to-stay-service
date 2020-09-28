838e1dddc555335b0ad2c8c327e608da0d1fa1ca
JacobDeCastroJohnson <Jacob.DeCastro.J@gmail.com>
2020-09-28 15:37:02 -0700

    Initialize Jest and Enzyme Test Environments with template confirmations. No Use case tests included

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,27 @@
-const path = require('path');
+/* eslint-disable no-path-concat */
+/* eslint-disable prefer-template */
+// const path = require('path');
 
 module.exports = {
-  entry: path.resolve(__dirname, '/client/src/index.jsx'),
+  // entry: path.resolve(__dirname, '/client/src/index.jsx'),
+  entry: __dirname + '/client/src/index.jsx',
   module: {
     rules: [
       {
-        test: /\.jsx$/,
+        test: [/\.jsx$/],
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env'],
+          },
         },
       },
     ],
   },
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, '/client/dist'),
+    // path: path.resolve(__dirname, '/client/dist'),
+    path: __dirname + '/client/dist',
   },
 };
